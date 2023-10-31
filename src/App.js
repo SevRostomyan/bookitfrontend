5699768956d7577ba9a2403352582fe1d6203a20
Parisa Norouzi <parisanorouzi93@gmail.com>
2023-10-31 21:57:36 +0100

    App.js refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import RegistrationForm from '../src/Components/Registration/ReqistrationForm';
-import BookingForm from '../src/Components/Booking/BookingForm';
+import Navbar from "./Components/nav/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/pages/Home";
+import Register from "./Components/pages/Register";
+import Booking from "./Components/pages/Booking";
+
 
 function App() {
     return (
         <div className="App">
-            <RegistrationForm />
-            <BookingForm />
+            <Navbar />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/booking" element={<Booking />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </main>
         </div>
     );
+
 }
 
 export default App;
