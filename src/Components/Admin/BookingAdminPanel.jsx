b9739101efda803d845db0c23726a3ba8ad18957
Parisa Norouzi <parisanorouzi93@gmail.com>
2023-12-18 18:07:25 +0100

    Jag har emplementerat funktioner för att ta bort bokningar.

--- src/Components/Admin/BookingAdminPanel.jsx
+++ src/Components/Admin/BookingAdminPanel.jsx
@@ -16,8 +16,39 @@ class BookingAdminPanel extends Component {
                 customerFilter: '',
                 typeFilter: '',
             },
+            selectedItemToDelete: null,
+            showDeleteConfirmation: false,
         };
     }
+
+    // Function to handle delete button click
+    handleDeleteClick = (item) => {
+        this.setState({
+            selectedItemToDelete: item,
+            showDeleteConfirmation: true,
+        });
+    };
+
+    // Function to handle confirm delete action
+    handleConfirmDelete = () => {
+        // Perform the delete action (e.g., call an API)
+        // ...
+
+        // After successful deletion, close the confirmation dialog
+        this.setState({
+            selectedItemToDelete: null,
+            showDeleteConfirmation: false,
+        });
+    };
+
+    // Function to handle cancel delete action
+    handleCancelDelete = () => {
+        // Cancel the delete action
+        this.setState({
+            selectedItemToDelete: null,
+            showDeleteConfirmation: false,
+        });
+    };
 
     resetFilters = () => {
         this.setState({
@@ -185,10 +216,18 @@ class BookingAdminPanel extends Component {
                                     <td className="text-left">{item.date}</td>
                                     <td className="text-left">{item.message}</td>
                                     <td className="text-center w-12">{item.price} kr</td>
-                                    <td>
-                                        <button className="button button-link">
+                                    <td className="flex">
+                                        {/* Edit btn */}
+                                        <button className="button button-link px-0">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                            </svg>
+                                        </button>
+                                        {/* Delete btn */}
+                                        <button className="button button-link button-link-danger px-0"
+                                                onClick={() => this.handleDeleteClick(item)}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                             </svg>
                                         </button>
                                     </td>
@@ -197,6 +236,17 @@ class BookingAdminPanel extends Component {
                             </tbody>
                         </table>
                     </div>
+
+                    {/* Delete confirmation dialog */}
+                    {this.state.showDeleteConfirmation && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <p>Är du säker?</p>
+                                <button className="button-danger mt-6" onClick={this.handleConfirmDelete}>Ja</button>
+                                <button className="button-secondary mt-4" onClick={this.handleCancelDelete}>Nej</button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         );
