a1cc2a88312d50a353d2c7db2d71a7debf7a313c
Nima <Nimamikaelghodsi@gmail.com>
2023-12-19 16:13:14 +0100

    Lade till AuthContext, uppdaterade fetch-metoder, lade till tabellstruktur för bokningar, lade till feedback samt kompletterande ändringar i CSS.

--- src/Components/Dashboard/BookingManagement.jsx
+++ src/Components/Dashboard/BookingManagement.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import {useAuth} from "../../AuthContext";
+import { useAuth } from "../../AuthContext";
 
 function BookingManagement() {
     const [bookings, setBookings] = useState([]);
     const [selectedBooking, setSelectedBooking] = useState(null);
     const [availableCleaners, setAvailableCleaners] = useState([]);
-    const { auth } = useAuth(); // Use the useAuth hook to access the auth state
+    const { auth } = useAuth();
 
     useEffect(() => {
         fetchNotAssignedBookings();
@@ -13,8 +13,7 @@ function BookingManagement() {
 
     const fetchNotAssignedBookings = async () => {
         try {
-            const token =auth.token; // Use the token from the AuthContext
-
+            const token = auth.token;
             const response = await fetch('http://localhost:7878/api/admin/fetchNotAssignedBookings', {
                 method: 'GET',
                 headers: {
@@ -33,16 +32,12 @@ function BookingManagement() {
 
     const fetchAvailableCleaners = async (bookingTime) => {
         try {
-
-            const token =auth.token; // Use the token from the AuthContext
-
-            const response = await fetch('http://localhost:7878/api/admin/available-cleaners', {
+            const token = auth.token;
+            const response = await fetch(`http://localhost:7878/api/admin/available-cleaners?bookingTime=${bookingTime}`, {
                 method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
-                },
-                body: JSON.stringify({ bookingTime })
+                }
             });
             if (!response.ok) {
                 throw new Error('Failed to fetch available cleaners');
@@ -56,19 +51,19 @@ function BookingManagement() {
 
     const assignCleaning = async (bookingId, cleanerId) => {
         try {
-            const token =auth.token; // Use the token from the AuthContext
+            const token = auth.token;
             const response = await fetch('http://localhost:7878/api/admin/assignCleaning', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}` // Include the token in the Authorization header
+                    'Authorization': `Bearer ${token}`
                 },
                 body: JSON.stringify({ bookingId, cleanerId })
             });
             if (!response.ok) {
                 throw new Error('Failed to assign cleaning');
             }
-            // Hantera framgångsrik tilldelning
+            alert('Cleaning successfully assigned!'); // Feedback
             fetchNotAssignedBookings(); // Uppdatera listan med ej tilldelade bokningar
         } catch (error) {
             console.error('Error:', error);
@@ -86,29 +81,40 @@ function BookingManagement() {
         }
     };
 
+    // Tabellstruktur för att visa bokningar
     return (
         <div>
             <h2>Bokningshantering</h2>
-            <div>
-                <h3>Ej tilldelade bokningar</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>Beskrivning</th>
+                    <th>Bokningstid</th>
+                    <th>Välj</th>
+                </tr>
+                </thead>
+                <tbody>
                 {bookings.map(booking => (
-                    <div key={booking.id} onClick={() => handleSelectBooking(booking)}>
-                        {booking.description} - {booking.bookingTime}
-                    </div>
+                    <tr key={booking.id}>
+                        <td>{booking.description}</td>
+                        <td>{booking.bookingTime}</td>
+                        <td><button onClick={() => handleSelectBooking(booking)}>Välj</button></td>
+                    </tr>
                 ))}
-            </div>
-            <div>
-                {selectedBooking && (
-                    <>
-                        <h3>Tillgängliga städare för vald bokning</h3>
+                </tbody>
+            </table>
+            {selectedBooking && (
+                <>
+                    <h3>Tillgängliga städare för vald bokning</h3>
+                    <ul>
                         {availableCleaners.map(cleaner => (
-                            <div key={cleaner.id} onClick={() => handleAssignCleaning(cleaner.id)}>
+                            <li key={cleaner.id} onClick={() => handleAssignCleaning(cleaner.id)}>
                                 {cleaner.name}
-                            </div>
+                            </li>
                         ))}
-                    </>
-                )}
-            </div>
+                    </ul>
+                </>
+            )}
         </div>
     );
 }
