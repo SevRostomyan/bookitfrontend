549cb9d982fe94cd4e14986ba688dc10ea9fa8e3
Nima <Nimamikaelghodsi@gmail.com>
2023-12-28 22:48:11 +0100

    Uppdaterade AdminDashboard komponenten för att fungera med de nya komponenterna. Samt korrigerade navigeringen.

--- src/Components/Dashboard/AdminDashboard.jsx
+++ src/Components/Dashboard/AdminDashboard.jsx
@@ -4,7 +4,6 @@ import '../../assets/Dashboard.css';
 import BookingManagement from './BookingManagement';
 import PaymentManagement from './PaymentManagement';
 import RegisterUserForm from './RegisterUserForm';
-import UsersList from './UsersList';
 import UserManagement from './UserManagement';
 
 function AdminDashboard() {
@@ -19,17 +18,16 @@ function AdminDashboard() {
         <div className="dashboard">
             <div className="dashboard-welcome">ADMINISTRATÖR</div>
             <div className="dashboard-options">
-                <button className="dashboard-button" onClick={() => navigateTo('users')}>Användare</button>
+                <button className="dashboard-button" onClick={() => navigateTo('user-management')}>Användare</button>
                 <button className="dashboard-button" onClick={() => navigateTo('bookings')}>Bokningar</button>
                 <button className="dashboard-button" onClick={() => navigateTo('payments')}>Betalningar</button>
                 <button className="dashboard-button" onClick={() => navigateTo('register')}>Registrera Användare</button>
             </div>
             <Routes>
-                <Route path="users" element={<UsersList />} />
+                <Route path="user-management" element={<UserManagement />} />
                 <Route path="bookings" element={<BookingManagement />} />
                 <Route path="payments" element={<PaymentManagement />} />
                 <Route path="register" element={<RegisterUserForm />} />
-                <Route path="user-management" element={<UserManagement />} />
             </Routes>
         </div>
     );
