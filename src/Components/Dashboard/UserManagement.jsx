92794a6c995e55988ee026803cb8d0fd521dbe24
Nima <Nimamikaelghodsi@gmail.com>
2023-12-28 22:39:12 +0100

    Lade till komponent för att hantera uppdatering av användare.

--- src/Components/Dashboard/UserManagement.jsx
+++ src/Components/Dashboard/UserManagement.jsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from "../../AuthContext";
+import UpdateUserModal from './UpdateUserModal';
 
 function UserManagement() {
     const [cleaners, setCleaners] = useState([]);
     const [customers, setCustomers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [showModal, setShowModal] = useState(false);
     const { auth } = useAuth();
 
     useEffect(() => {
-        fetchUsers('städare', setCleaners);
-        fetchUsers('kunder', setCustomers);
+        refreshUsers();
     }, []);
 
-    const fetchUsers = async (userType, setUserList) => {
+    const refreshUsers = () => {
+        fetchUsers('städare/all', setCleaners);
+        fetchUsers('kunder/all', setCustomers);
+    };
+
+    const fetchUsers = async (endpoint, setUserList) => {
         try {
-            const endpoint = userType === 'städare' ? 'städare/all' : 'kunder/all';
             const token = auth.token;
 
             const response = await fetch(`http://localhost:7878/api/admin/${endpoint}`, {
@@ -23,10 +29,56 @@ function UserManagement() {
                 }
             });
             if (!response.ok) {
-                throw new Error(`Failed to fetch ${userType}`);
+                throw new Error('Failed to fetch users');
             }
             const data = await response.json();
             setUserList(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleDeleteUser = async (userId, userType) => {
+        try {
+            const token = auth.token;
+            const response = await fetch(`http://localhost:7878/api/admin/${userType}/delete`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ userId })
+            });
+            if (!response.ok) {
+                throw new Error('Failed to delete user');
+            }
+            refreshUsers(); // Uppdatera listan med användare
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleUpdateUser = (user) => {
+        setSelectedUser(user);
+        setShowModal(true);
+    };
+
+    const onUpdateUser = async (updatedUser) => {
+        try {
+            const token = auth.token;
+            const response = await fetch(`http://localhost:7878/api/admin/users/update`, {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(updatedUser)
+            });
+            if (!response.ok) {
+                throw new Error('Failed to update user');
+            }
+            setShowModal(false);
+            refreshUsers(); // Uppdatera listan med användare
         } catch (error) {
             console.error('Error:', error);
         }
@@ -38,15 +90,24 @@ function UserManagement() {
             <div>
                 <h3>Städare</h3>
                 {cleaners.map(cleaner => (
-                    <div key={cleaner.id}>{cleaner.name}</div>
+                    <div key={cleaner.id}>
+                        {cleaner.name}
+                        <button onClick={() => handleDeleteUser(cleaner.id, 'städare')}>Ta bort</button>
+                        <button onClick={() => handleUpdateUser(cleaner)}>Uppdatera</button>
+                    </div>
                 ))}
             </div>
             <div>
                 <h3>Kunder</h3>
                 {customers.map(customer => (
-                    <div key={customer.id}>{customer.name}</div>
+                    <div key={customer.id}>
+                        {customer.name}
+                        <button onClick={() => handleDeleteUser(customer.id, 'kund')}>Ta bort</button>
+                        <button onClick={() => handleUpdateUser(customer)}>Uppdatera</button>
+                    </div>
                 ))}
             </div>
+            {showModal && <UpdateUserModal user={selectedUser} onClose={() => setShowModal(false)} onUpdate={onUpdateUser} />}
         </div>
     );
 }
