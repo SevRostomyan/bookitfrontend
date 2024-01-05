8ef6e50df55c579c11ab8231e34758b4c796138c
Parisa Norouzi <parisanorouzi93@gmail.com>
2024-01-05 16:09:02 +0100

    Har emplementerat funktioner för att redigera bokningar för administera bokningar

--- src/Components/Admin/BookingAdminPanel.jsx
+++ src/Components/Admin/BookingAdminPanel.jsx
@@ -63,14 +63,18 @@ class BookingAdminPanel extends Component {
 
         if (this.state.editingItem) {
 
+            console.log(this.state.items)
+
             // Update the existing item
             const updatedItems = this.state.items.map(item =>
                 item.id === this.state.editingItem.id ? { ...item, ...this.state.newItem } : item
             );
 
-            this.setState(prevState => ({
+            console.log(updatedItems)
+
+            this.setState({
                 items: updatedItems,
-            }));
+            });
         } else {
             // Add the new item to the items array
             this.items.push({
@@ -99,9 +103,9 @@ class BookingAdminPanel extends Component {
     };
 
     // Function to handle delete button click
-    handleDeleteClick = (item) => {
+    handleDeleteClick = (index) => {
         this.setState({
-            selectedItemToDelete: item,
+            selectedItemToDelete: index,
             showDeleteConfirmation: true,
         });
     };
@@ -109,10 +113,12 @@ class BookingAdminPanel extends Component {
     // Function to handle confirm delete action
     handleConfirmDelete = () => {
         // Perform the delete action (e.g., call an API)
-        // ...
+        const updatedItems = [...this.state.items];
+        updatedItems.splice(this.state.selectedItemToDelete, 1);
 
         // After successful deletion, close the confirmation dialog
         this.setState({
+            items: updatedItems,
             selectedItemToDelete: null,
             showDeleteConfirmation: false,
         });
@@ -445,7 +451,7 @@ class BookingAdminPanel extends Component {
                                         </button>
                                         {/* Delete btn */}
                                         <button className="button button-link button-link-danger px-0"
-                                                onClick={() => this.handleDeleteClick(item)}>
+                                                onClick={() => this.handleDeleteClick(index)}>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                             </svg>
