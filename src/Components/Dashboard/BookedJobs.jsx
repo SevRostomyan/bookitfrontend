f9688ec25b0961ce3c19b11892b22aca26c00d1d
SevRostomyan <41015978+SevRostomyan@users.noreply.github.com>
2024-02-11 18:00:04 +0100

    La till en till tabell i komponenten Booked Jobbs för avslutade uppdrag. Modifierade kolumnerna samt la till anpassade meddelanden och felhantering vid avsaknad av data.

--- src/Components/Dashboard/BookedJobs.jsx
+++ src/Components/Dashboard/BookedJobs.jsx
@@ -4,23 +4,26 @@ import { useAuth } from "../../AuthContext";
 const BookedJobs = () => {
     const [notStartedJobs, setNotStartedJobs] = useState([]);
     const [inProgressJobs, setInProgressJobs] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
+    const [reportedCompletedJobs, setReportedCompletedJobs] = useState([]);
+    const [isLoadingReportedCompleted, setIsLoadingReportedCompleted] = useState(false);
+    const [errorReportedCompleted, setErrorReportedCompleted] = useState('');
+
+    const [isLoadingNotStarted, setIsLoadingNotStarted] = useState(false);
+    const [isLoadingInProgress, setIsLoadingInProgress] = useState(false);
+    const [errorNotStarted, setErrorNotStarted] = useState('');
+    const [errorInProgress, setErrorInProgress] = useState('');
     const { auth } = useAuth();
 
     useEffect(() => {
-        fetchAllJobs();
+        fetchJobs('fetchNotStartedBookingsByUserId', setNotStartedJobs, setIsLoadingNotStarted, setErrorNotStarted);
+        fetchJobs('fetchInProgressBookingsByUserId', setInProgressJobs, setIsLoadingInProgress, setErrorInProgress);
+        fetchJobs('fetchReportedCompletedBookingsByUserId', setReportedCompletedJobs, setIsLoadingReportedCompleted, setErrorReportedCompleted);
     }, []);
 
-    const fetchAllJobs = async () => {
+    const fetchJobs = async (endpoint, setState, setIsLoading, setError) => {
         setIsLoading(true);
         setError('');
 
-        await fetchJobs('fetchNotStartedBookingsByUserId', setNotStartedJobs);
-        await fetchJobs('fetchInProgressBookingsByUserId', setInProgressJobs);
-    };
-
-    const fetchJobs = async (endpoint, setState) => {
         try {
             const response = await fetch(`http://localhost:7878/api/bokning/${endpoint}`, {
                 method: 'POST',
@@ -30,17 +33,18 @@ const BookedJobs = () => {
                 },
             });
 
+            if (response.status === 204) {
+                // Handle 204 No Content response
+                setState([]);
+                return;
+            }
+
             if (!response.ok) {
                 throw new Error(`Failed to fetch jobs from ${endpoint}`);
             }
 
-            const contentType = response.headers.get('content-type');
-            if (contentType && contentType.indexOf('application/json') !== -1) {
-                const data = await response.json();
-                setState(data);
-            } else {
-                throw new Error('No JSON content in response');
-            }
+            const data = await response.json();
+            setState(data);
         } catch (error) {
             setError(error.message);
         } finally {
@@ -65,7 +69,8 @@ const BookedJobs = () => {
             }
 
             alert('Cleaning started successfully');
-            fetchAllJobs();
+            fetchJobs('fetchNotStartedBookingsByUserId', setNotStartedJobs, setIsLoadingNotStarted, setErrorNotStarted);
+            fetchJobs('fetchInProgressBookingsByUserId', setInProgressJobs, setIsLoadingInProgress, setErrorInProgress);
         } catch (error) {
             console.error('Error:', error);
             alert('Failed to start cleaning');
@@ -88,43 +93,43 @@ const BookedJobs = () => {
             }
 
             alert('Cleaning reported as completed successfully');
-            fetchAllJobs();
+            fetchJobs('fetchNotStartedBookingsByUserId', setNotStartedJobs, setIsLoadingNotStarted, setErrorNotStarted);
+            fetchJobs('fetchInProgressBookingsByUserId', setInProgressJobs, setIsLoadingInProgress, setErrorInProgress);
         } catch (error) {
             console.error('Error:', error);
             alert('Failed to report cleaning as completed');
         }
     };
 
-    return (
-        <div>
-            <h2>Inbokade Jobb</h2>
-            {isLoading && <div>Loading...</div>}
-            {error && <div>Error: {error}</div>}
-            {!isLoading && !error && (
-                <>
-                    {renderJobsTable("Not Started Jobs", notStartedJobs, startCleaning, 'Starta')}
-                    {renderJobsTable("In Progress Jobs", inProgressJobs, finishCleaning, 'Avsluta')}
-                </>
-            )}
-        </div>
-    );
-};
+    const renderNotStartedJobsTable = () => {
+        return renderJobsTable("Not Started Jobs", notStartedJobs, startCleaning, 'Starta');
+    };
 
-const renderJobsTable = (title, jobs, actionHandler, actionButtonText) => {
-    return (
-        <>
-            <h3>{title}</h3>
-            {jobs.length > 0 ? (
+    const renderJobsTable = (title, jobs, actionHandler, actionButtonText, isLoading, error, noDataMessage) => {
+        if (isLoading) {
+            return <div>Loading...</div>;
+        }
+        if (error) {
+            return <div>Error: {error}</div>;
+        }
+        if (jobs.length === 0) {
+            return <p>{noDataMessage}</p>;
+        }
+
+        return (
+            <>
+                <h3>{title}</h3>
                 <table>
                     <thead>
                     <tr>
                         <th>ID</th>
                         <th>Kund</th>
-                        <th>Städare</th>
+                        <th>Bokningsdress</th>
                         <th>Tjänst</th>
                         <th>Bokningstid</th>
+                        <th>Kundmeddelande</th>
                         <th>Status</th>
-                        <th>Åtgärd</th>
+                        {title !== "Reported Completed Jobs" && <th>Åtgärd</th>}
                     </tr>
                     </thead>
                     <tbody>
@@ -132,21 +137,35 @@ const renderJobsTable = (title, jobs, actionHandler, actionButtonText) => {
                         <tr key={job.id}>
                             <td>{job.id}</td>
                             <td>{job.kund.firstname} {job.kund.lastname}</td>
-                            <td>{job.städare ? `${job.städare.firstname} ${job.städare.lastname}` : 'Ej tilldelad'}</td>
+                            <td>{job.adress}</td>
                             <td>{job.tjänst}</td>
                             <td>{job.bookingTime}</td>
+                            <td>{job.messageAtBooking}</td>
                             <td>{job.status}</td>
-                            <td>
-                                <button onClick={() => actionHandler(job.id)}>{actionButtonText}</button>
-                            </td>
+                            {title !== "Reported Completed Jobs" && (
+                                <td>
+                                    {actionHandler && <button onClick={() => actionHandler(job.id)}>{actionButtonText}</button>}
+                                </td>
+                            )}
                         </tr>
                     ))}
                     </tbody>
                 </table>
-            ) : (
-                <p>Det finns inga inbokade jobb just nu.</p>
-            )}
-        </>
+            </>
+        );
+    };
+
+
+    return (
+        <div>
+            <h2>Inbokade Jobb</h2>
+            {renderJobsTable("", notStartedJobs, startCleaning, 'Starta', isLoadingNotStarted, errorNotStarted, "Det finns inga ej påbörjade uppdrag just nu.")}
+            <h2>Påbörjade uppdrag</h2>
+            {renderJobsTable("", inProgressJobs, finishCleaning, 'Avsluta', isLoadingInProgress, errorInProgress, "Det finns inga påbörjade uppdrag just nu.")}
+            <h2>Avslutade uppdrag</h2>
+            {renderJobsTable("", reportedCompletedJobs, null, '', isLoadingReportedCompleted, errorReportedCompleted, "Det finns inga avslutade uppdrag just nu.")}
+        </div>
     );
 };
-export default BookedJobs
+
+export default BookedJobs;
