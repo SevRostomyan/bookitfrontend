b63e5636a6e4c8e54244e31ffd51a32278e44184
Nima Ghodsi <nimamikaelghodsi@gmail.com>
2024-03-09 18:06:25 +0100

    Uppdaterade mina bokningar fetch

--- src/Components/Dashboard/CustomerDashboard.jsx
+++ src/Components/Dashboard/CustomerDashboard.jsx
@@ -29,7 +29,21 @@ const CustomerDashboard = () => {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
+                data.forEach(booking => {
+                    console.log(`Booking ID: ${booking.id}`);
+                    console.log(`Customer: ${booking.kund.firstname} ${booking.kund.lastname}, Email: ${booking.kund.email}`);
+                    console.log(`Cleaner: ${booking.städare.firstname} ${booking.städare.lastname}, Email: ${booking.städare.email}`);
+                    console.log(`Service: ${booking.tjänst}`);
+                    console.log(`Booking Time: ${booking.bookingTime}`);
+                    console.log(`End Time: ${booking.endTime}`);
+                    console.log(`Address: ${booking.adress}`);
+                    console.log(`Message At Booking: ${booking.messageAtBooking}`);
+                    console.log(`Cleaning Reported Time: ${booking.cleaningReportedTime}`);
+                    console.log(`Customer Feedback: ${booking.customerFeedback}`);
+                    console.log(`Status: ${booking.status}`);
+                    console.log(`Cleaning Report Status: ${booking.cleaningReportStatus}`);
+                    console.log('------------------------------------');
+                });
             })
             .catch(error => {
                 console.error('Error fetching bookings:', error);
@@ -39,7 +53,6 @@ const CustomerDashboard = () => {
     const handleHistoryClick = () => {
         navigate('/history');
     };
-
         const handlePaymentMethodsClick = () => {
             const apiUrl = '/api/betalning'; 
 
