f7595494e371b57f969eac1d33575d435e059133
Nima Ghodsi <nimamikaelghodsi@gmail.com>
2024-03-09 17:42:27 +0100

    Lade till headers i booking management

--- src/Components/Dashboard/BookingManagement.jsx
+++ src/Components/Dashboard/BookingManagement.jsx
@@ -13,7 +13,8 @@ function BookingManagement() {
             const response = await fetch('http://localhost:7878/api/admin/fetchNotAssignedBookings', {
                 method: 'GET',
                 headers: {
-                    'Authorization': `Bearer ${token}`
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
                 }
             });
             if (!response.ok) {
